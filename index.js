3697e0da3e7705f82b493fd1eec8a35d0ef42652
Jacob Sarnowski <hello@jsarnowski.com>
2022-10-24 17:55:12 +0000

    wip

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log("Hello World");
+const fetch = require('isomorphic-fetch')
+
+const config = {
+    target: {
+        id: 'T4_2H_CLAYMORE',
+        tier: 3
+    },
+    dependencies: [
+        { item: '', have: 0 },
+        { item: '', have: 0 },
+    ]
+}
+
+const fetchItem = async itemId => {
+    const response = await fetch(`https://gameinfo.albiononline.com/api/gameinfo/items/${itemId}/data`)
+    return response.json()
+}
+
+const getItemEnchantments = (item, tier) => {
+    return item.enchantments.enchantments.find(({ enchantmentLevel }) => enchantmentLevel === tier)
+}
+
+const getTopLevelCraftingRequirements = (item, tier) => {
+    const enchantment = getItemEnchantments(item, tier);
+    return enchantment ? enchantment.craftingRequirements.craftResourceList : null
+}
+
+const getCraftingRequirements = craftingRequirements => {
+
+}
+
+const calculateInvestment = async () => {
+    const item = await fetchItem(config.target.id)
+
+    // const response = await fetch('https://www.albion-online-data.com/api/v2/stats/prices/T4_BAG,T5_BAG.json?locations=Caerleon,Bridgewatch&qualities=2');
+
+    const topLevelRequirements =  getTopLevelCraftingRequirements(item, config.target.tier)
+    // const lowLevelRequirements = topLevelRequirements.map(convertTopLevelCraftingRequirementsToLowLevel)
+
+    const test = await fetchItem('T4_METALBAR_LEVEL1@1');
+
+    console.log({ test })
+}
+
+calculateInvestment();
